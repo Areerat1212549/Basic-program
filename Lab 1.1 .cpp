b1f15e3f15ca1f24e89eb50823abb19960aea6a5
Areerat <arirat1212549@gmail.com>
2025-11-26 15:57:24 +0700

    Add files via upload

---
+++ Lab 1.1 .cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+    int N, i ;
+
+
+    if (scanf ("%d",&N)  !=1 ) {
+        return 1;
+    } //end if
+       {
+         for (int i =0; i < N; i++)
+        {
+            printf("Hello Loop!\n");
+        }   // end for 
+        if (N<=0){
+
+        printf("Error");}
+
+     }
+
+
+    return 0;
+}
